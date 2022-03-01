d5383a3ed6351626d4178492289842692e1179e0
Egor <egorlavrinovich@gmail.com>
2022-03-01 16:18:10 +0300

    burger_menu

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
-const moto  = document.querySelector('.moto'),
+const burgerMenu  = document.querySelector('.header__menu'),
+      burgerClick = document.querySelector('.header__burger'),
+      ScrollBody = document.getElementsByClassName('body'),
       StyleMoto = document.querySelector('.gallery__image img')
-moto.addEventListener('click',(e)=>{
-    e.preventDefault()
+
+burgerClick.addEventListener('click',()=>{
+    burgerMenu.classList.toggle('active');
+    burgerClick.classList.toggle('active');
+    ScrollBody.classList.toggle('active');
+})
+
+$(document).ready(function(){
+    $('.header__burger').click(function(event){
+        $('body').toggleClass('lock')
     })
+})
